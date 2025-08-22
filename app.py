f428bed05d8f83fb11f7b833f37ed60cf838c3d3
Andrea Silva <andreassilvas@hotmail.com>
2025-08-21 22:15:46 -0400

    age updated

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from style_config import (
 # ===================
 etre_vivants = [
     Animal("Lion", 15, "Mammifère", "Carnivore"),
-    Animal("Perroquet", 11, "Oiseau", "Herbivore"),
+    Animal("Perroquet", 15, "Oiseau", "Herbivore"),
     Plante("Lotus", 8, "Large", "Argentée"),
     Plante("Aloe", 15, "Épineuse", "Jaune"),
 ]
